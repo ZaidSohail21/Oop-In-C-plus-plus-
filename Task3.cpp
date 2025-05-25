5de36153cf15b8e8129cb01d05b29ef6766ddbb8
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 20:57:56 +0500

    Cource Inrollment System using Inheritane

--- Task3.cpp
+++ Task3.cpp
@@ -1,76 +1,95 @@
 //#include <iostream>
 //using namespace std;
-//class Complex{
-//	double real;
-//	double imaginary;
+//class Student {
+//private:
+//	char name[50];
+//	int age;
+//
 //public:
-//	Complex(){
-//		cout << "Default constructor" << endl;
-//		real = 0;
-//		imaginary = 0;
-//	}
-//	Complex(double r, double i){
-//		cout << "Partialized Construstor " << endl;
-//		real = 0;
-//		imaginary = 0;
-//	}
-//	//setter
-//	void setValue(){
-//		cout << "Enter the real number  :";
-//		cin >> real;
-//		cout << "Enter the Imaginary number  :";
-//		cin >> imaginary;
-//	}
-//	//getter
-//	int getreal(){
-//		return real;
-//	}
-//	int getimaginary(){
-//		return imaginary;
-//	}
-//	// Displaying Results
-//	void display(){
-//		cout << "Real number :" << real << endl;
-//		cout << "Imaginary Number :" << imaginary << endl;
-//	}
-//	void sum(double w, double x, double y, double z){
-//		double a = w + y;
-//		double b = x + z;
-//		cout << "the sum of two complex number : " << a << endl;
-//		cout << "the sum of two imaginary number : " << b << endl;
-//	}
-//	void dif(double w, double x, double y, double z){
-//		double a = w - y;
-//		double b = x - z;
-//		cout << "The difference of two complex number : " << a << endl;
-//		cout << "The difference of two imaginary number : " << b << endl;
+//	Student(const char* n, int a) {
+//		int i = 0;
+//		while (n[i] != '\0' && i < 49) {
+//			name[i] = n[i];
+//			i++;
+//		}
+//		name[i] = '\0';
+//		age = a;
 //	}
 //
+//	void displayInfo() const {
+//		std::cout << "  Name: " << name << ", Age: " << age << std::endl;
+//	}
 //};
-//int main(){
-//	Complex com1;
-//	double a, b;
-//	cout << "1st complex number " << endl;
-//	com1.setValue();
-//	a=com1.getreal();
-//	b = com1.getimaginary();
-//	// display
-//	com1.display();
 //
-//	double c, d;
-//	Complex com2;
-//	cout << "2nd complex number " << endl;
-//	com2.setValue();
-//	c = com2.getreal();
-//	d = com2.getimaginary();
-//	// display
-//	com2.display();
-//	com1.sum(a, b, c, d);
-//	com1.dif(a, b, c, d);
+//class Teacher {
+//private:
+//	char name[50];
+//	Student* students[10];
+//	int studentCount;
+//	const int maxStudents;
+//
+//public:
+//	Teacher(const char* n, int max) : studentCount(0), maxStudents(max) {
+//		int i = 0;
+//		while (n[i] != '\0' && i < 49) {
+//			name[i] = n[i];
+//			i++;
+//		}
+//		name[i] = '\0';
+//	}
+//
+//	bool addStudent(const Student& student) {
+//		if (studentCount < maxStudents) {
+//			students[studentCount] = new Student(student);
+//			studentCount++;
+//			return true;
+//		}
+//		else {
+//			std::cout << "Maximum student capacity reached for " << name << "." << std::endl;
+//			return false;
+//		}
+//	}
+//
+//	void display() const {
+//		std::cout << "Teacher: " << name << std::endl;
+//		if (studentCount == 0) {
+//			std::cout << "No students assigned yet." << std::endl;
+//			return;
+//		}
+//		std::cout << "Students:" << std::endl;
+//		for (int i = 0; i < studentCount; i++) {
+//			students[i]->displayInfo();
+//		}
+//	}
+//	~Teacher(){
+//		for (int i = 0; i< studentCount; i++){
+//			delete students[i];
+//		}
+//	}
+//};
+//
+//int main() {
+//	Teacher teacher1("Mr. Smith", 3);
+//
+//	Student student1("Alice", 18);
+//	Student student2("Bob", 19);
+//	Student student3("Charlie", 20);
+//	Student student4("David", 21);
+//	teacher1.addStudent(student1);
+//	teacher1.addStudent(student2);
+//	teacher1.addStudent(student3);
+//	teacher1.addStudent(student4);
 //
 //
+//	Teacher teacher2("Ms. Johnson", 2);
+//	Student student5("Eve", 19);
+//	Student student6("Frank", 20);
+//	teacher2.addStudent(student5);
+//	teacher2.addStudent(student6);
 //
+//	teacher1.display();
 //	cout << endl;
+//	teacher2.display();
 //	system("pause");
 //	return 0;
 //}
