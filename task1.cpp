6159f2643a6d79bbb8d22d944f0b02ec01dc4e91
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 20:46:24 +0500

    Operator Overloading

--- task1.cpp
+++ task1.cpp
@@ -1,72 +1,91 @@
 //#include <iostream>
+//#include <string>
 //using namespace std;
+//class Rectangle {
+//	double length;
+//	double width;
 //
-//void CompareArea(){
-//
-//
-//}
-//class rectangle {
-//	int length;
-//	int width;
 //public:
-//	rectangle(){
-//		cout << "Default constructor " << endl;
-//		length = 0;
-//		width = 0;
-//	}
-//	rectangle(int l, int w){
-//		cout << "Paramatric constructor " << endl;
-//		length = l;
-//		width = w;
-//	}
-//	rectangle(rectangle &ra){
-//		cout << "Shallow constructor " << endl;
-//		length = ra.length;
-//		width = ra.width;
-//	}
-//	//setter
-//	void setter(int l,int w){
-//		length = l;
-//		width = w;
-//	}
-//	//gettter
-//	void lengthgetter(){ cout << length; }
-//	void widthgetter(){ cout << width; }
-//	int area(){ return length*width; }
-//	//display function
+//	// Default Constructor
+//	Rectangle() : length(0), width(0) { }
+//
+//	// Parameterized Constructor
+//	Rectangle(double a,double b) : length(a), width(b) { }
+//	//copy constructor
+//	Rectangle(Rectangle& obj) :length(obj.length), width(obj.width){ }
+//	// Setters
+//	void numbersetter(double a) { length = a; }
+//	void balancesetter(double b) { width = b; }
+//	
+//
+//	// Getters
+//	double lengthgetter() const { return length; }
+//	double widthgetter() const { return width; }
+//	// Display
 //	void display(){
-//		cout << "Length : ";
-//		lengthgetter();
-//		cout << endl;
-//		int z=area();
-//		cout << "Width : ";
-//		widthgetter();
-//		cout << endl;
-//		cout << "ARea :" << z << endl;
+//		cout << "Length :" << lengthgetter() << endl;
+//		cout << "Width :" << widthgetter() << endl;
 //	}
-//
-//
-//	// Deleting object
-//	~rectangle(){
-//		cout << "Default constructor " << endl;
+//	//operators
+//	Rectangle operator+(const Rectangle& other){
+//		Rectangle temp;
+//		temp.length = this->length + other.length;
+//		temp.width = this->width + other.width;
+//		return temp;
 //	}
+//	Rectangle operator*(const Rectangle& other){
+//		Rectangle temp;
+//		temp.length =length * other.length;
+//		temp.width = width * other.width;
+//		return temp;
+//	}
+//	Rectangle operator*(int scalar){
+//		
+//		this->length = this->length*scalar;
+//		this->width = this->width*scalar;
+//		return *this;
+//	}
+//	bool operator==(const Rectangle& other){
+//		if (this->length == other.length && this->width == other.width){
+//			return true;
+//		}
+//		else{
+//			return false;
+//		}
+//	}
+//	bool operator!=(const Rectangle& other){
+//		if (this->length == other.length && this->width == other.width){
+//			return false;
+//		}
+//		else{
+//			return true;
+//		}
+//	}
+//	friend ostream& operator << (ostream& out, const Rectangle& obj);
 //
 //};
-//int main(){
-//	int a, b;
-//	rectangle r1;
-//	cout << "Enter length :";
-//	cin >> a;
-//	cout << "Enter width :";
-//	cin >> b;
-//	r1.setter(a, b);
-//	rectangle r2(23, 43);
-//	rectangle r3 = r2;
-//	r1.display();
-//	r2.display();
+//ostream& operator << (ostream& out, const Rectangle& obj){
+//	out << "length :" << obj.length << endl;
+//	out << "width :" << obj.width << endl;
+//	return out;
+//}
+//int main() {
+//	int x;
+//	Rectangle r1;
+//	Rectangle r2(2, 5);
+//	Rectangle r3=r1 + r2;
 //	r3.display();
-//
+//	r3 * 2;
+//	cout << r3;
+//	r3*r2;
+//	if (r2 == r3){
+//		cout << "They are equal" << endl;
+//	}
+//	else{
+//		cout << "They are not equal" << endl;
+//	}
 //	cout << endl;
 //	system("pause");
 //	return 0;
 //}
+
