9edf4466992089f84424c98cef331c2ab26cad4c
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 20:01:55 +0500

    Classes attributes and methods (Student Class)

--- Task2.cpp
+++ Task2.cpp
@@ -1,46 +1,92 @@
-#include <iostream>
-using namespace std;
-
-int Area(int l){
-	int s = l*l;
-	return s;
-}
-int Area(int b, int h){
-	int w = b*h;
-	return w / 2;
-}
-double Area(double r){
-	int cir = r*r;
-	return 3.14*cir;
-}
-
-int main(){
-	double length, base,height;
-	double radius;
-	cout << "Enter the Length :";
-	cin >> length;
-
-	cout << "Enter the Base :";
-	cin >> base;
-
-	cout << "Enter the Height :";
-	cin >> height;
-
-	cout << "Enter the Radius :";
-	cin >> radius;
-	cout << "The area of a square is :";
-	int square = Area(length);
-	cout << square << endl;
-
-	cout << "The area of a Triangle is :";
-	int triangle = Area(base, height);
-	cout << triangle << endl;
-
-	cout << "The area of a circle is :";
-	double circle = Area(radius);
-	cout << circle << endl;
-
-	cout << endl;
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Student
+//{
+//	int studentID;
+//	double GPA;
+//	string degreeName, studentName;
+//public:
+//	void display()
+//	{
+//		cout << "Enter the Student ID: ";
+//		cin >> studentID;
+//		cout << "Enter your Name: ";
+//		cin >> studentName;
+//		cout << "Enter the Dergree: ";
+//		cin >> degreeName;
+//		cout << "Enter the GPA: ";
+//		cin >> GPA;
+//	}
+//	double calculatePercentage(double total, double obt)
+//	{
+//		double per = (obt / total) * 100;
+//		return per;
+//	}
+//	void disPercantage(double Percentage)
+//	{
+//
+//		if (Percentage >= 86)
+//		{
+//			cout << "The Grade is 'A+'.";
+//		}
+//		else if (Percentage >= 82)
+//		{
+//			cout << "The Grade is 'A'.";
+//		}
+//		else if (Percentage >= 78)
+//		{
+//			cout << "The Grade is 'B+'.";
+//		}
+//		else if (Percentage >= 74)
+//		{
+//			cout << "The Grade is 'B'.";
+//		}
+//		else if (Percentage >= 70)
+//		{
+//			cout << "The Grade is 'B-'.";
+//		}
+//		else if (Percentage >= 66)
+//		{
+//			cout << "The Grade is 'C+'.";
+//		}
+//		else if (Percentage >= 62)
+//		{
+//			cout << "The Grade is 'C'.";
+//		}
+//		else if (Percentage >= 58)
+//		{
+//			cout << "The Grade is 'C-'.";
+//		}
+//		else if (Percentage >= 54)
+//		{
+//			cout << "The Grade is 'D+'.";
+//		}
+//		else if (Percentage >= 50)
+//		{
+//			cout << "The Grade is 'D'.";
+//		}
+//		else if (Percentage <50)
+//		{
+//			cout << "The Grade is 'F'.";
+//		}
+//
+//	}
+//};
+//int main()
+//{
+//	Student s1;
+//	s1.display();
+//
+//	double total, obtain;
+//	cout << "Enter Your obtained marks: ";
+//	cin >> obtain;
+//	cout << "Enter Your total marks: ";
+//	cin >> total;
+//
+//	double Percentage = s1.calculatePercentage(total, obtain);
+//	s1.disPercantage(Percentage);
+//	cout << endl;
+//	system("pause");
+//	return 0;
+//}
