f21fd4e109749980514add2d8f9a55c017f9885e
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 21:10:53 +0500

    Use of Templates in Class

--- task1.cpp
+++ task1.cpp
@@ -1,91 +1,36 @@
 //#include <iostream>
-//#include <string>
 //using namespace std;
-//class Rectangle {
-//	double length;
-//	double width;
 //
-//public:
-//	// Default Constructor
-//	Rectangle() : length(0), width(0) { }
-//
-//	// Parameterized Constructor
-//	Rectangle(double a,double b) : length(a), width(b) { }
-//	//copy constructor
-//	Rectangle(Rectangle& obj) :length(obj.length), width(obj.width){ }
-//	// Setters
-//	void numbersetter(double a) { length = a; }
-//	void balancesetter(double b) { width = b; }
-//	
-//
-//	// Getters
-//	double lengthgetter() const { return length; }
-//	double widthgetter() const { return width; }
-//	// Display
-//	void display(){
-//		cout << "Length :" << lengthgetter() << endl;
-//		cout << "Width :" << widthgetter() << endl;
-//	}
-//	//operators
-//	Rectangle operator+(const Rectangle& other){
-//		Rectangle temp;
-//		temp.length = this->length + other.length;
-//		temp.width = this->width + other.width;
-//		return temp;
-//	}
-//	Rectangle operator*(const Rectangle& other){
-//		Rectangle temp;
-//		temp.length =length * other.length;
-//		temp.width = width * other.width;
-//		return temp;
-//	}
-//	Rectangle operator*(int scalar){
-//		
-//		this->length = this->length*scalar;
-//		this->width = this->width*scalar;
-//		return *this;
-//	}
-//	bool operator==(const Rectangle& other){
-//		if (this->length == other.length && this->width == other.width){
-//			return true;
-//		}
-//		else{
-//			return false;
-//		}
-//	}
-//	bool operator!=(const Rectangle& other){
-//		if (this->length == other.length && this->width == other.width){
-//			return false;
-//		}
-//		else{
-//			return true;
-//		}
-//	}
-//	friend ostream& operator << (ostream& out, const Rectangle& obj);
-//
-//};
-//ostream& operator << (ostream& out, const Rectangle& obj){
-//	out << "length :" << obj.length << endl;
-//	out << "width :" << obj.width << endl;
-//	return out;
+//template <typename T>
+//void mySwap(T& a, T& b) {
+//	T temp = a;
+//	a = b;
+//	b = temp;
 //}
+//
 //int main() {
-//	int x;
-//	Rectangle r1;
-//	Rectangle r2(2, 5);
-//	Rectangle r3=r1 + r2;
-//	r3.display();
-//	r3 * 2;
-//	cout << r3;
-//	r3*r2;
-//	if (r2 == r3){
-//		cout << "They are equal" << endl;
-//	}
-//	else{
-//		cout << "They are not equal" << endl;
-//	}
-//	cout << endl;
+//	int x = 5, y = 10;
+//	float p = 1.5f, q = 2.5f;
+//	char c1 = 'A', c2 = 'B';
+//	double d1 = 3.14, d2 = 2.71;
+//
+//	cout << "Before swapping: " << endl;
+//	cout << "int: " << x << " " << y << endl;
+//	cout << "float: " << p << " " << q << endl;
+//	cout << "char: " << c1 << " " << c2 << endl;
+//	cout << "double: " << d1 << " " << d2 << endl;
+//
+//	mySwap(x, y);
+//	mySwap(p, q);
+//	mySwap(c1, c2);
+//	mySwap(d1, d2);
+//
+//	cout << "After swapping: " << endl;
+//	cout << "int: " << x << " " << y << endl;
+//	cout << "float: " << p << " " << q << endl;
+//	cout << "char: " << c1 << " " << c2 << endl;
+//	cout << "double: " << d1 << " " << d2 << endl;
+//	cout << "hi" << endl;
 //	system("pause");
 //	return 0;
 //}
-
