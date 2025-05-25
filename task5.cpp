8ffcb036f9a823341606f0e25c480118314571ef
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 21:12:05 +0500

    use of Template in Dynammic arrays

--- task5.cpp
+++ task5.cpp
@@ -1,240 +1,125 @@
 #include <iostream>
+#include <string>
 using namespace std;
-class rectangle {
-	int length;
-	int width;
+
+// Template class for dynamic array
+template <typename T>
+class Array {
+private:
+	T* arr;
+	int maxSize;
+	int currentSize;
+
 public:
-	rectangle(){
-		cout << "Default constructor " << endl;
-		length = 0;
-		width = 0;
+	// Constructor
+	Array(int size) {
+		maxSize = size;
+		currentSize = 0;
+		arr = new T[maxSize];
 	}
-	rectangle(int l, int w){
-		cout << "Paramatric constructor " << endl;
-		length = l;
-		width = w;
+
+	// Destructor
+	~Array() {
+		delete[] arr;
 	}
-	rectangle(rectangle &ra){
-		cout << "Shallow constructor " << endl;
-		length = ra.length;
-		width = ra.width;
+
+	// Function to remove an element (remove from the end)
+	T remove() {
+		if (currentSize > 0) {
+			currentSize--;
+			return arr[currentSize];
+		}
+		else {
+			throw runtime_error("Array is empty");
+		}
 	}
-	//setter
-	void setter(int l,int w){
-		length = l;
-		width = w;
+
+	// Function to insert an element
+	void insert(T value) {
+		if (!isFull()) {
+			arr[currentSize++] = value;
+		}
+		else {
+			throw runtime_error("Array is full");
+		}
 	}
-	//gettter
-	void lengthgetter(){ cout << length; }
-	void widthgetter(){ cout << width; }
-	int area(){ return length*width; }
-	//display function
-	void display(){
-		cout << "Length : ";
-		lengthgetter();
+
+	// Function to display the elements of the array
+	void display() const {
+		if (isEmpty()) {
+			cout << "Array is empty." << endl;
+			return;
+		}
+		for (int i = 0; i < currentSize; i++) {
+			cout << arr[i] << " ";
+		}
 		cout << endl;
-		int z=area();
-		cout << "Width : ";
-		widthgetter();
-		cout << endl;
-		cout << "ARea :" << z << endl;
 	}
 
-
-	// Deleting object
-	~rectangle(){
-		cout << "Default constructor " << endl;
+	// Function to check if the array is full
+	bool isFull() const {
+		return currentSize == maxSize;
 	}
 
-};
-class person{
-	string* name=new string;
-	int age;
-public:
-	person(){
-		cout << "Default constructor " << endl;
-		*name = 'null';
-		age = 0;
-	}
-	person(string* l, int w){
-		cout << "Paramatric constructor " << endl;
-		*name = *l;
-		age= w;
-	}
-	/*person(person &ra){
-		cout << "Shallow constructor " << endl;
-		name = ra.name;
-		age = ra.age;
-	}*/
-	person(person &ra){
-		cout << "Deep constructor " << endl;
-		name =new string( *ra.name);
-		age = ra.age;
-	}
-	//setter
-	void setter(string *nam, int w){
-		this->name = nam;
-		this->age = age;
-	}
-	//gettter
-	void namegetter(){ cout << *name; }
-	void agegetter(){ cout << age; }
-	
-	//display function
-	void display(){
-		cout << "Name  : ";
-		namegetter();
-		cout << endl;
-		
-		cout << "Age  : ";
-		agegetter();
-		cout << endl;
-		
-	}
-
-
-	// Deleting object
-	~person(){
-		cout << "Default destructor " << endl;
-	}
-
-};
-class student{
-	string* name=new string;
-	int age;
-	int id;
-public:
-	student(){
-		cout << "Default constructor " << endl;
-		*name = 'null';
-		age = 0;
-		id = 0;
-	}
-	student(string* l, int w,int i){
-		cout << "Paramatric constructor " << endl;
-		*name = *l;
-		age= w;
-		id = i;
-	}
-	 student(student &ra){
-		cout << "Shallow constructor " << endl;
-		name = ra.name;
-		age = ra.age;
-		id = ra.id;
-
-	}
-	/*student(student &ra){
-		cout << "Deep constructor " << endl;
-		name =new string( *ra.name);
-		age = ra.age;
-		id = ra.id;
-	}*/
-	//setter
-	void setter(string *nam, int age, int id ){
-		this->name = nam;
-		this->age = age;
-		this->id = id;
-	}
-	//gettter
-	void namegetter(){ cout << *name; }
-	void agegetter(){ cout << age; }
-	void idgetter(){ cout << id; }
-	
-	//display function
-	void display(){
-		cout << "Name  : ";
-		namegetter();
-		cout << endl;
-		
-		cout << "grade  : ";
-		agegetter();
-		cout << endl;
-
-		cout << "ID : ";
-		idgetter();
-		cout << endl;	
-	}
-	// Deleting object
-	~student(){
-		delete []name;
-		cout << "Default destructor " << endl;
+	// Function to check if the array is empty
+	bool isEmpty() const {
+		return currentSize == 0;
 	}
 };
 
-int main(){
-	int a = 0;
-	do{
-		cout << "press 1 for rectangle" << endl;
-		cout << "press 2 for person" << endl;
-		cout << "press 3 for student " << endl;
-		cout << "press 4 for book " << endl;
-		cout << "press 0 for exit " << endl;
-		cin >> a;
-		if (a = 1){
-				int a, b;
-				rectangle r1;
-				cout << "Enter length :";
-				cin >> a;
-				cout << "Enter width :";
-				cin >> b;
-				r1.setter(a, b);
-				rectangle r2(23, 43);
-				rectangle r3 = r2;
-				r1.display();
-				r2.display();
-				r3.display();
-			
-		}
-		else if (a = 2){
-				int a;
-				string *ptr = new string[1];
-				person p1;
-				cout << "Enter name :";
-				cin >> *ptr ;
-				cout << "Enter age :";
-				cin >> a;
-				p1.setter(ptr, a);
-			
-				cout << "Enter name :";
-				cin >> *ptr;
-				cout << "Enter age :";
-				cin >> a;
-				person p2;
-				p2.setter(ptr, a);
-				cout << "P1 display :" << endl;
-				p1.display();
-			
-				cout << "P2 display :" << endl;
-				p2.display();
-				
-		}
-		else if (a = 3){
-				int a,b;
-				string *ptr = new string[1];
-				student s[3];
-				for (int i = 0; i < 2; i++){
-					cout << "Enter name :";
-					cin >> *ptr;
-					cout << "Enter grade :";
-					cin >> a;
-					cout << "Enter ID :";
-					cin >> b;
-					s[i].setter(ptr, a, b);
-				}
-				for (int i = 0; i < 3; i++){
-					s[i].display();
-				}
-				cout << "Now after copying constructor" << endl;
-				s[1] = s[2];
-			
-				for (int i = 0; i < 3; i++){
-					s[i].display();
-				}
-		}
-		else{
-			cout << "Enter a valid number! " << endl;
-		}
-	}while (a != 0);
+int main() {
+	// Test with int type
+	Array<int> intArray(5);
+	intArray.insert(10);
+	intArray.insert(20);
+	intArray.insert(30);
+	cout << "Integer array: ";
+	intArray.display();
+	intArray.remove();
+	cout << "After removing an element: ";
+	intArray.display();
+
+	// Test with char type
+	Array<char> charArray(3);
+	charArray.insert('A');
+	charArray.insert('B');
+	cout << "\nCharacter array: ";
+	charArray.display();
+	charArray.remove();
+	cout << "After removing an element: ";
+	charArray.display();
+
+	// Test with char* type
+	Array<const char*> stringArray(4);
+	stringArray.insert("Hello");
+	stringArray.insert("World");
+	cout << "\nString array: ";
+	stringArray.display();
+	stringArray.remove();
+	cout << "After removing an element: ";
+	stringArray.display();
+
+	// Test with string type
+	Array<string> stringObjArray(3);
+	stringObjArray.insert("Apple");
+	stringObjArray.insert("Banana");
+	cout << "\nString object array: ";
+	stringObjArray.display();
+	stringObjArray.remove();
+	cout << "After removing an element: ";
+	stringObjArray.display();
+
+	// Test with float type
+	Array<float> floatArray(5);
+	floatArray.insert(3.14f);
+	floatArray.insert(2.71f);
+	cout << "\nFloat array: ";
+	floatArray.display();
+	floatArray.remove();
+	cout << "After removing an element: ";
+	floatArray.display();
+	cout << "hi" << endl;
 	system("pause");
 	return 0;
 }
+
