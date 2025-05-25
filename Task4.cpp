16ee5cf994b8a3996a68ea871789100a7c3fca6b
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 20:55:36 +0500

    Herarical Inheritance

--- Task4.cpp
+++ Task4.cpp
@@ -1,63 +1,87 @@
-#include <iostream>
-#include <string>
-using namespace std;
-class Vehicle{
-public:
-	string brand;
-	int regno;
-
-
-	Vehicle() :brand("null"), regno(0){ cout << "Sports Constructor " << endl; }
-	Vehicle(string n, int i) :brand(n), regno(i){ cout << "Sports Constructor " << endl; }
-	//setter
-	void setbrand(string b){ brand=b; }
-	void setregno(int r){  regno=r; }
-	//getter
-	string getbrand(){ return brand; }
-	int getregno(){ return regno; }
-	//display
-	void display(){
-		cout << "Name :" << brand << endl;
-		cout << "ID :" << regno << endl;
-	}
-};
-class RoadVehicle{
-public:
-	
-	int noofwheel;
-	//setter
-	void setnoof(int w){ noofwheel=w; }
-	//getter
-	int noof(){ return noofwheel; }
-
-
-	RoadVehicle() :noofwheel(0){ cout << "Sports Constructor " << endl; }
-	RoadVehicle(int i) :noofwheel(i){ cout << "Sports Constructor " << endl; }
-
-	//display
-	void display(){
-		cout << "No Of Wheel :" << noofwheel << endl;
-	}
-};
-class car :public Vehicle,public RoadVehicle{
-public:
-	string color;
-	int miles;
-	//setter
-	void setcolor(string c){  color=c; }
-	void setmiles(int m){  miles=m; }
-	//getter
-	string getcolor(){ return color; }
-	int getmiles(){ return miles; }
-
-	car() :Vehicle(), RoadVehicle(), color("null"), miles(0){}
-	car(string c, int m,string b,int r,int w ) :color(c), miles(m),Vehicle(c,r),RoadVehicle(w){}
-	
-};
-
-int main(){
-	car c1("white",2000,"toyota",1234,10);
-	
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//class property{
+//	float size;
+//public:
+//	// constructor 
+//	property() : size(0){}
+//	property(float s) : size(s){}
+//	//display,
+//	void display(){
+//		cout << "Size :" << size << endl;
+//	}
+//};
+//class owner{
+//	int id;
+//	string name;
+//public:
+//	//constructor
+//	owner(int i, string n) :id(i), name(n){ }
+//	// display
+//	void displayOwner(){
+//		cout << "Id :" << id << endl;
+//		cout << "Name :" << name << endl;
+//	}
+//};
+//class  ResidentialProperty :public property{
+//	int bednum;
+//	bool hasgarden;
+//public:
+//	ResidentialProperty(float s, int b, bool ba) :property(s), bednum(b), hasgarden(ba){ }
+//	//display
+//	void displayResidentialProperty(){
+//		cout << "Numbers of bed room is :" << bednum << endl;
+//		if (hasgarden == true){
+//			cout << "Yes! it has a garden " << endl;
+//		}
+//		else{
+//			cout << "No! it not have a garden" << endl;
+//		}
+//	}
+//};
+//class CommercialProperty :public property{
+//	string buisnesstype;
+//	int NoFloors;
+//public:
+//	CommercialProperty(float s, int i, string n) :property(s), NoFloors(i), buisnesstype(n){ }
+//	// display
+//	void displayCommercialProperty(){
+//		cout << "NoFloors :" << NoFloors << endl;
+//		cout << "buisnesstype :" << buisnesstype << endl;
+//	}
+//
+//};
+//class RentalProperty :public ResidentialProperty, public owner{
+//	double rentalPrice;
+//public:
+//
+//	RentalProperty(float s, int bedrooms, bool garden, int id, string n, double price) : ResidentialProperty(s, bedrooms, garden), owner(id, n), rentalPrice(price) {}
+//
+//	void displayRentalProperty() {
+//		displayOwner();
+//		displayResidentialProperty();
+//		cout << "Rental Price: $" << rentalPrice << std::endl;
+//	}
+//};
+//
+//int main(){
+//
+//	ResidentialProperty residential(150, 3, true);
+//	cout << "--- Residential Property ---" << std::endl;
+//	residential.displayResidentialProperty();
+//	cout << endl;
+//
+//	CommercialProperty commercial(150, "Retail", 2);
+//	cout << "--- Commercial Property ---" << endl;
+//	commercial.displayCommercialProperty();
+//	cout << endl;
+//
+//	RentalProperty rental(100, 2, false, 123, "John Doe", 1200.0);
+//	cout << "--- Rental Property ---" << endl;
+//	rental.displayRentalProperty();
+//
+//	cout << endl;
+//	system("pause");
+//	return 0;
+//}
