5c38d3083a12ae1a292f5e48335420a25509ba07
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 20:00:49 +0500

    Rectangle Class to demostrate the attribut concept

---
+++ Task1.cpp
@@ -0,0 +1,36 @@
+//#include<iostream>
+//using namespace std;
+//class rec
+//{
+//	double length;
+//	double width;
+//public:
+//	void display()
+//	{
+//		cout << "Enter the width: ";
+//		cin >> width;
+//		cout << "Enter the length: ";
+//		cin >> length;
+//	}
+//	void setValue()
+//	{
+//		cout << "The lenght is: " << width << endl;
+//		cout << "The width is: " << length << endl;
+//	}
+//	double calArea()
+//	{
+//		double area = length*width;
+//		return area;
+//
+//	}
+//};
+//int main()
+//{
+//	rec name;
+//	name.display();
+//	name.setValue();
+//	double var = name.calArea();
+//	cout << "The area is: " << var << endl;
+//	system("pause");
+//	return 0;
+//}
