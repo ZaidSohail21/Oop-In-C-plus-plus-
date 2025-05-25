59437875fddda148aad0e654903ed4c2384906f1
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 20:48:42 +0500

    Single inheritance (Shape->rectangle)

--- Task1.cpp
+++ Task1.cpp
@@ -1,36 +1,37 @@
-//#include<iostream>
+//#include <iostream>
+//#include <string>
 //using namespace std;
-//class rec
-//{
-//	double length;
-//	double width;
+//class shape{
 //public:
-//	void display()
-//	{
-//		cout << "Enter the width: ";
-//		cin >> width;
-//		cout << "Enter the length: ";
-//		cin >> length;
-//	}
-//	void setValue()
-//	{
-//		cout << "The lenght is: " << width << endl;
-//		cout << "The width is: " << length << endl;
-//	}
-//	double calArea()
-//	{
-//		double area = length*width;
-//		return area;
+//	string color;
+//	int side;
 //
+//	shape() :color("null"), side(0){ }
+//	shape(string c, int s) :color(c), side(s){}
+//	//getter
+//	string getcolor(){ return color; }
+//	int getside(){ return side; }
+//	//display
+//	void display(){
+//		cout << "Color :" << color << endl;
+//		cout << "Side :" << side << endl;
 //	}
 //};
-//int main()
-//{
-//	rec name;
-//	name.display();
-//	name.setValue();
-//	double var = name.calArea();
-//	cout << "The area is: " << var << endl;
+//class rectangle :public shape{
+//public:
+//	rectangle() :shape(){}
+//	void display(shape s1){
+//		cout <<"Color :"<< s1.color << endl;
+//		cout <<"Side :"<< s1.side << endl;
+//	}
+//
+//
+//};
+//int main(){
+//	shape s1;
+//	rectangle r1;
+//	s1.display();
+//	r1.display(s1);
 //	system("pause");
 //	return 0;
 //}
