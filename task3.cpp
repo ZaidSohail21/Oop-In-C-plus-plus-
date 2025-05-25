fdd54e57d66117f435f6029c74dd52d0c5572161
ZaidSohail21 <zaidsohail4321@gmail.com>
2025-05-25 20:50:55 +0500

    Multi-Level Inheritance (Sport->team->player)

--- task3.cpp
+++ task3.cpp
@@ -1,109 +1,50 @@
 //#include <iostream>
 //#include <string>
 //using namespace std;
-//class Rectangle {
-//	int x, y;
+//class Sports{
 //public:
-//	// Default Constructor
-//	Rectangle() : x(0), y(0) { }
+//	string name;
+//	int id;
+//	
 //
-//	// Parameterized Constructor
-//	Rectangle(int a, int b) : x(a), y(b) { }
-//	//copy constructor
-//	Rectangle(Rectangle& obj) :x(obj.x), y(obj.y){ }
-//	// Setters
-//	void xsetter(int a) { x = a; }
-//	void ysetter(int b) { y = b; }
+//	Sports() :name("null"), id(0){ cout << "Sports Constructor " << endl; }
+//	Sports(string n, int i) :name(n), id(i){ cout << "Sports Constructor " << endl; }
 //
-//
-//	// Getters
-//	int xgetter() const { return x; }
-//	int ygetter() const { return y; }
-//	// Display
+//	//display
 //	void display(){
-//		cout << "Imaginary :" << xgetter() << endl;
-//		cout << "Real :" << ygetter() << endl;
+//		cout << "Name :" << name << endl;
+//		cout << "ID :" << id << endl;
 //	}
-//	//operators
-//	Rectangle operator+(const Rectangle& other){
-//		Rectangle Temp;
-//		Temp.x = this->x + other.x;
-//		Temp.y = this->y + other.y;
-//		cout << Temp.x << endl;
-//		cout << Temp.y << endl;
-//		return Temp;
-//	}
-//	Rectangle operator-(const Rectangle& other){
-//		Rectangle Temp;
-//		Temp.x = this->x - other.x;
-//		Temp.y = this->y - other.y;
-//		cout << Temp.x << endl;
-//		cout << Temp.y << endl;
-//		return Temp;
-//	}
-//
-//	bool operator==(const Rectangle& other){
-//		if (this->y == other.y && this->x == other.x){
-//			return true;
-//		}
-//		else{
-//			return false;
-//		}
-//	}
-//	bool operator!=(const Rectangle& other){
-//		if (this->x == other.x && this->y == other.y){
-//			return false;
-//		}
-//		else{
-//			return true;
-//		}
-//	}
-//	Rectangle operator++(){
-//		++x;
-//		++y;
-//		return *this;
-//	}
-//	Rectangle operator++(int){
-//		x++;
-//		y++;
-//		return *this;
-//	}
-//	Rectangle operator--(){
-//		--x;
-//		--y;
-//		return *this;
-//	}
-//	Rectangle operator--(int){
-//		x--;
-//		y--;
-//		return *this;
-//	}
-//	friend ostream& operator << (ostream& out, const Rectangle& obj);
-//
 //};
-//ostream& operator << (ostream& out, const Rectangle& obj){
-//	out << "length :" << obj.x << endl;
-//	out << "width :" << obj.y << endl;
-//	return out;
-//}
-//int main() {
-//	Rectangle r1;
-//		Rectangle r2(1, 2);
-//		Rectangle r5(2, 3);
-//		Rectangle r3=r5 + r2;
-//		cout << r3;
-//		
-//		Rectangle r4 = r5- r2;
-//		cout << r4;
-//		if (r4 == r1){
-//			cout << "they are equal" << endl;
-//		}
-//		else{
-//			cout << "They are not equal" << endl;
-//		}
-//		r4++;
-//		r4--;
-//	cout << endl;
+//class Team :public Sports{
+//public:
+//	string teamname;
+//	Team() :Sports(),teamname("null") { cout << "Team  Constructor " << endl; }
+//	Team(string n, int i,string t ) :Sports(n, i),teamname(t){ cout << "Team  Constructor " << endl; }
+//};
+//class Player:public Team{
+//public:
+//	string name;
+//	int years;
+//
+//
+//	Player() :name("null"), years(0){ cout << "Player Constructor " << endl; }
+//	Player(string name,int id,string t,string n, int i) :Team(name,id,t),name(n), years(i){ cout << "Player Constructor " << endl; }
+//
+//	//display
+//	void display(Sports s1,Team t1){
+//		cout << "SportsName :" << s1.name << endl;
+//		cout << "Playerid :" << s1.id << endl;
+//		cout << "Teamname :" << t1.teamname << endl;
+//		cout << "PLayer Name :" << name << endl;
+//		cout << "Expyears :" << years << endl;
+//	}
+//};
+//int main(){
+//	Sports s1("cricket", 0314);
+//	Team t1("cricket", 0314,"lahore");
+//	Player p1("cricket", 0314, "lahore", "zaid", 5);
+//	p1.display(s1, t1);
 //	system("pause");
 //	return 0;
 //}
